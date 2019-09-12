9f4ba3ce156086456cc66766a2f336ec2cecb7e8
genix9876 <54391269+genix9876@users.noreply.github.com>
2019-09-12 14:30:11 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -14,7 +14,7 @@ namespace test{
     export function sayhello(test_: string) {
         basic.showString(test_)
     }
-    //% blockId=printxyz weight 60 blockGap = 30
+    //% blockId=printxyz
     //% block = "led at pin %p | digital write %v"
     //% v.min = 0 v.max = 1 x.defl = 0
     export function test1(p: DigitalPin, v: number) {
