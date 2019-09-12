692a30d120e17b35180032f491721add617e5004
genix9876 <54391269+genix9876@users.noreply.github.com>
2019-09-12 14:33:11 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -9,14 +9,14 @@ basic.showLeds(`
 
 //% color = #008000 
 namespace test{
-    //% blockId = printname 
+    //% blockId=printname 
     //% block="Enter your name %test_"
     export function sayhello(test_: string) {
         basic.showString(test_)
     }
     //% blockId=printxyz
-    //% block = "led at pin %p | digital write %v"
-    //% v.min = 0 v.max = 1 x.defl = 0
+    //% block="led at pin %p | digital write %v"
+    //% v.min=0 v.max=1 x.defl=0
     export function test1(p: DigitalPin, v: number) {
         pins.digitalWritePin(p, v)
     }       
