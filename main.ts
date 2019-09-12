cfd329eb73d8b0d2336565575d3bd0660b7b5cf3
genix9876 <54391269+genix9876@users.noreply.github.com>
2019-09-12 14:29:21 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -9,11 +9,15 @@ basic.showLeds(`
 
 //% color = #008000 
 namespace test{
-    //% blockId = print weight 60 blockGap = 30
-    //% block = 'led at pin %p | digital write %v'
+    //% blockId = printname 
+    //% block="Enter your name %test_"
+    export function sayhello(test_: string) {
+        basic.showString(test_)
+    }
+    //% blockId=printxyz weight 60 blockGap = 30
+    //% block = "led at pin %p | digital write %v"
     //% v.min = 0 v.max = 1 x.defl = 0
     export function test1(p: DigitalPin, v: number) {
-        pins.digitalWritePin(p, v);
-        
+        pins.digitalWritePin(p, v)
     }       
-    }
+}
